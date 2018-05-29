e374b2232ab204e02bc4257791502c27e3d59646
masolar <masonlary@gmail.com>
2018-05-29 20:33:20 +0530

    Working on adding alpha functions to this branch

--- src/main/scala/functions/ArgFunctionBase.scala
+++ src/main/scala/functions/ArgFunctionBase.scala
@@ -3,5 +3,5 @@ package functions
 import java.awt.Color
 
 trait ArgFunctionBase {
-  def getPixel(x: Float, y: Float): Seq[Float]
+  def getPixel(x: Float, y: Float, alpha: Float): Seq[Float]
 }
--- src/main/scala/functions/FuncGenerator.scala
+++ src/main/scala/functions/FuncGenerator.scala
@@ -7,21 +7,23 @@ import scala.util.Random
   * Creates a function for generating images
   */
 object FuncGenerator {
-  val leaves: Seq[(Int) => ZeroArgFunction] = Seq((i) => XFunction(i), (i) => YFunction(i))
-  val branches = _
+  val leaves: Seq[() => ZeroArgFunction] = Seq(() => XFunction(), () => YFunction())
+  val branches = ???
 
-  def generateFunction(minDepth: Int, maxDepth: Int): ZeroArgFunction = {
-    def generateFunctionHelper(minDepth: Int, maxDepth: Int, currDepth: Int) = {
+  def generateFunction(minDepth: Int, maxDepth: Int) = {
+    def generateFunctionHelper(minDepth: Int, maxDepth: Int, currDepth: Int): ArgFunctionBase = {
       if (currDepth == maxDepth) {
         getRandomFunction(leaves) match {
-          case one_arg: ((Int) => ZeroArgFunction) => one_arg(Random.nextFloat())
+          case one_arg: (() => ZeroArgFunction) => one_arg()
+          case _ => XFunction()
         }
       }
+      generateFunctionHelper(minDepth, maxDepth, currDepth + 1)
     }
     generateFunctionHelper(minDepth, maxDepth, 1)
   }
 
-  def generateFunction(functionString: String)
+  //def generateFunction(functionString: String) = ???
 
   def getRandomFunction(functions: Seq[Any]) = {
     functions(Random.nextInt(functions.length))
